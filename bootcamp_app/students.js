a68e0d116255f75659083707ac3e22473c0d7b67
Gurulakshmi Varadharaj <gurulakshmi.varadharaj@gmail.com>
2020-09-24 05:00:21 +0000

    SQL injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,6 +1,9 @@
 'user-strict'
 
-const queryParam = process.argv.slice(2);
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
 
 const { Pool } = require('pg');
 
@@ -11,20 +14,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS student_name, cohorts.name AS cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '${queryParam[0]}%'
-LIMIT ${queryParam[1]};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as student_name, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.student_name} has an id of ${user.student_id} and was in the ${user.cohort_name} cohort`);
     });
   })
   .catch(err => console.log('query error', err.stack));
-
-pool.query(`
-
-`)
